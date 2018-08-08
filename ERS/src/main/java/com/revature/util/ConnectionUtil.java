bfd3c24f8ec1deaf44a98a1b76ca166b5bbf8cb0
Kevin Wu <kwu.uic@gmail.com>
2018-08-08 12:27:47 -0400

    changed xml

--- ERS/src/main/java/com/revature/util/ConnectionUtil.java
+++ ERS/src/main/java/com/revature/util/ConnectionUtil.java
@@ -21,30 +21,20 @@ public final class ConnectionUtil {
 	}
 
 	public static Connection getConnection() throws SQLException{
-		InputStream in = null;
 
 		try {
-			Properties props = new Properties();
-			in = new FileInputStream("C:/Workspace/ERS/src/main/resources/connection.properties");
-			props.load(in);
 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			String endpoint = props.getProperty("jdbc:oracle:thin:@kwudb.cpmpgeabndob.us-east-1.rds.amazonaws.com:1521:ORCL");
-			String username = props.getProperty("kevin");
-			String password = props.getProperty("52790Zsnxu");
 
-			return DriverManager.getConnection(endpoint, username, password);
+			return DriverManager.getConnection("jdbc:oracle:thin:@kwudb.cpmpgeabndob.us-east-1.rds.amazonaws.com:1521:ORCL", "kevin", "52790Zsnxu");
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
+	
+		
 
 		return null;
-	}
+	
+	} 
 }
