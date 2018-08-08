cc32cc32e6d45badea0afed0ba83dad9a2e84252
Kevin Wu <kwu.uic@gmail.com>
2018-08-08 12:24:07 -0400

    second change to properties

--- ERS/src/main/java/com/revature/util/ConnectionUtil.java
+++ ERS/src/main/java/com/revature/util/ConnectionUtil.java
@@ -25,14 +25,14 @@ public final class ConnectionUtil {
 
 		try {
 			Properties props = new Properties();
-			in = new FileInputStream("../ERS/src/main/resources/connection.properties");
+			in = new FileInputStream("C:/Workspace/ERS/src/main/resources/connection.properties");
 			props.load(in);
 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			String endpoint = props.getProperty("jdbc.url");
-			String username = props.getProperty("jdbc.username");
-			String password = props.getProperty("jdbc.password");
+			String endpoint = props.getProperty("jdbc:oracle:thin:@kwudb.cpmpgeabndob.us-east-1.rds.amazonaws.com:1521:ORCL");
+			String username = props.getProperty("kevin");
+			String password = props.getProperty("52790Zsnxu");
 
 			return DriverManager.getConnection(endpoint, username, password);
 		} catch (Exception e) {
